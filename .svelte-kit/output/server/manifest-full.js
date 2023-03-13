42fdde0132238b51cd8b072d7c8c59d88ca51af4
andrei.dumitrache <andrei.dumitrache@emag.ro>
2023-03-13 18:02:56 +0200

    add some base

--- .svelte-kit/output/server/manifest-full.js
+++ .svelte-kit/output/server/manifest-full.js
@@ -4,7 +4,7 @@ export const manifest = {
 	assets: new Set([".nojekyll","CNAME","favicon.png"]),
 	mimeTypes: {".png":"image/png"},
 	_: {
-		client: {"start":{"file":"_app/immutable/entry/start.f9742acd.js","imports":["_app/immutable/entry/start.f9742acd.js","_app/immutable/chunks/index.847936d9.js","_app/immutable/chunks/singletons.bd6fd6a5.js"],"stylesheets":[],"fonts":[]},"app":{"file":"_app/immutable/entry/app.a46de7f9.js","imports":["_app/immutable/entry/app.a46de7f9.js","_app/immutable/chunks/index.847936d9.js"],"stylesheets":[],"fonts":[]}},
+		client: {"start":{"file":"_app/immutable/entry/start.deaf24f6.js","imports":["_app/immutable/entry/start.deaf24f6.js","_app/immutable/chunks/index.847936d9.js","_app/immutable/chunks/singletons.8fa4c693.js"],"stylesheets":[],"fonts":[]},"app":{"file":"_app/immutable/entry/app.32661bcc.js","imports":["_app/immutable/entry/app.32661bcc.js","_app/immutable/chunks/index.847936d9.js"],"stylesheets":[],"fonts":[]}},
 		nodes: [
 			() => import('./nodes/0.js'),
 			() => import('./nodes/1.js'),
