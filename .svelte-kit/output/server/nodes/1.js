01aa5654fba0709984dac5c2d8d2f5f53729b9f2
7soapte <andrei.dumitrache@hotmail.com>
2023-03-13 17:44:53 +0200

    Delete .svelte-kit directory

--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
-export const file = '_app/immutable/entry/error.svelte.37fbcfc6.js';
-export const imports = ["_app/immutable/entry/error.svelte.37fbcfc6.js","_app/immutable/chunks/index.847936d9.js","_app/immutable/chunks/singletons.bd6fd6a5.js"];
-export const stylesheets = [];
-export const fonts = [];
