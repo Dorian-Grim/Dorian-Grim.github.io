1f55b00a9f43c2652426840fa51daa793000d1ee
andrei.dumitrache <andrei.dumitrache@emag.ro>
2023-03-13 17:11:56 +0200

    Test

--- gh-pages.js
+++ gh-pages.js
@@ -1,13 +1,13 @@
 import { publish } from "gh-pages";
 
 publish(
-  "build", // path to public directory
+  "build",
   {
     branch: "gh-pages",
-    repo: "https://github.com/7soapte/utm-info-quizz.git", // Update to point to your repository
+    repo: "https://github.com/7soapte/utm-info-quizz.git",
     user: {
-      name: "Andrei Dumitrache", // update to use your name
-      email: "andrei.dumitrache@hotmail.com", // Update to use your email
+      name: "Andrei Dumitrache",
+      email: "andrei.dumitrache@hotmail.com",
     },
     dotfiles: true,
   },
