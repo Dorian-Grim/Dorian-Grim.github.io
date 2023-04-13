4b06bf8e6f07efa6685a29ba7b722cac01b54146
Dorian <irinel.buraga@orange.com>
2023-04-13 12:22:30 +0300

    reading jsons dynamically

--- src/data/trivia.js
+++ src/data/trivia.js
@@ -1,43 +1,28 @@
-import algoritmi_structuri_date from "./JSONS/algoritmi si structuri de date.json";
-import baze_de_date from "./JSONS/baze de date.json";
-import comert_electronic from "./JSONS/comert electronic.json";
-import criptografie from "./JSONS/criptografie.json";
-import cloud_computing from "./JSONS/cloud_computing.json";
-import programare_in_c from "./JSONS/programare in c++.json";
-import programare_in_java from "./JSONS/programare in java.json";
-import programare_procedurala from "./JSONS/programare_procedurala.json"
-import retele_de_calculatoare from "./JSONS/retele de calculatoare.json";
-import sgbd from "./JSONS/sgbd.json";
-import sisteme_de_operare from "./JSONS/sisteme de operare.json";
-import tehnici_avansate_programare from "./JSONS/tehnici avansate de programare.json";
-import tehnologii_web from "./JSONS/tehnologii web.json";
-
-export const trivia = {
-  algoritmi_structuri_date,
-  baze_de_date,
-  comert_electronic,
-  cloud_computing,
-  criptografie,
-  programare_in_c,
-  programare_in_java,
-  programare_procedurala,
-  retele_de_calculatoare,
-  sgbd,
-  sisteme_de_operare,
-  tehnici_avansate_programare,
-  tehnologii_web,
-};
+const JSONS = import.meta.glob("./JSONS/*.json");
+/**
+ * @type any
+ */
+const trivia = {}
+for (const jsonPATH in JSONS) 
+{
+  const a = await import(/* @vite-ignore */jsonPATH);
+  trivia[jsonPATH.split("/")[2].split(".")[0].replaceAll(" ", "_")] = a.default
+}
 
 /**
  * @type {any[]}
  */
 export const trimmedTrivia = [];
 
-Object.entries(trivia).map(([, value]) => {
-  value.forEach((/** @type {any} */ element) => trimmedTrivia.push(element));
+Object.entries(trivia).map(([, value]) => 
+{
+  value.forEach((/** @type {any} */ element) => 
+  {
+    trimmedTrivia.push(element)
+  });
 });
-
-export const triviaForCourse = (/** @type {string} */ course) => {
+export const triviaForCourse = (/** @type {string} */ course) => 
+{
   if (course === "random") return trimmedTrivia;
   // @ts-ignore
   return trivia[course] ? trivia[course] : null;
