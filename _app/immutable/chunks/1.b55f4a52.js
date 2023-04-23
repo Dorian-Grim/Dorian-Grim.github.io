14b65f57003a9f186dc9e492728cbb1e8ebe5d3c
Dorian <buragairineldorian@gmail.com>
2023-04-23 13:35:14 +0300

    Updates

--- _app/immutable/chunks/1.b55f4a52.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.a9c62a79.js";export{t as component};
--- _app/immutable/chunks/2.9a0acf1b.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.74344440.js";export{t as component};
